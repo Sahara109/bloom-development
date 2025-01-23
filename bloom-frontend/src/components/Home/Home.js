60117f39011c05a922cbd7b6b702e2d7d0a52353
Sahara109 <np03cs4a220366@heraldcollege.edu.np>
2025-01-23 19:06:13 +0545

    Working on Frontend Features of BLOOM

--- bloom-frontend/src/components/Home/Home.js
+++ bloom-frontend/src/components/Home/Home.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import heroImage from "../../assets/images/image.png";
+import panicImage from "../../assets/images/panic_attack.png"; 
 import "./Home.css";
 
 const Home = () => {
@@ -17,13 +18,10 @@ const Home = () => {
           </div>
         </div>
         <div className="hero-image">
-          <img
-            src={heroImage} // Image path from assets
-            alt="Mental health and wellbeing"
-            className="hero-img" 
-          />
+          <img src={heroImage} alt="Mental health and wellbeing" className="hero-img" />
         </div>
       </section>
+
 
       {/* About Us Section */}
       <section className="about-section" id="about">
@@ -34,7 +32,29 @@ const Home = () => {
           an AI chatbot, we aim to support individuals on their journey toward
           mental wellbeing.
         </p>
-        <Link to="/about" className="btn btn-primary">Learn More</Link> {/* CTA */}
+        <Link to="/about" className="btn btn-primary">Learn More</Link>
+      </section>
+
+
+
+
+       {/* Panic Attack Description Section */}
+       <section className="panic-section">
+        <div className="panic-image">
+          <img src={panicImage} alt="Panic Attack Experience" className="panic-img" />
+        </div>
+        <div className="panic-content">
+          <h2>A Glimpse into Panic Attacks</h2>
+          <p>
+            "Tears welled in my eyes. My heart raced... I thought I was having a medical emergency. 
+            I thought I was dying.
+          </p>
+          <p>
+            The great irony was that to anyone just walking past, all they would have seen 
+            was a girl standing in a foyer."
+          </p>
+          <p><em>- Milli describes what a panic attack feels like</em></p>
+        </div>
       </section>
 
       {/* Features Section */}
@@ -59,6 +79,22 @@ const Home = () => {
           </div>
         </div>
       </section>
+
+     {/* Important Message Section */}
+<section className="important-message-section">
+  <h2>Important Message</h2>
+  <p>
+    This website has been established to provide information about anxiety, depression, 
+    and suicide to the community where the information about mental health is scarce. 
+    The website is not intended to be a substitute for professional medical advice, 
+    diagnosis, or treatment. You should seek the advice of an appropriately qualified 
+    healthcare professional before making decisions about your own circumstances. You 
+    should not disregard professional medical advice, or delay seeking it, because of 
+    any information contained on this website.
+  </p>
+</section>
+
+
     </div>
   );
 };
