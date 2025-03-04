1ccc147e8636ee6945921232416107319ed7a4b8
Sahara109 <np03cs4a220366@heraldcollege.edu.np>
2025-03-04 15:10:05 +0545

    BLOOM Module 4 Work

--- bloom-frontend/src/components/About/About.js
+++ bloom-frontend/src/components/About/About.js
@@ -112,7 +112,7 @@ const About = () => {
       </section>
       {/* Call to Action Section */}
       <section className="cta-section">
-        <p>Whether you are seeking information; looking for support or just need a moment of calm, BLOOM is here for you. Join us on this journey towards better mental health and well-being. Become a part of BLOOM today!</p>
+        <p>Whether you are seeking information, looking for support or just need a moment of calm, BLOOM is here for you. Join us on this journey towards better mental health and well-being. Become a part of BLOOM today!</p>
         <button className="cta-button">Get Started</button>
       </section>
     </div>
