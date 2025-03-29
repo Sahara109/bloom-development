50ba30b6ee3e8d11fdbee99e1f7b920d3bd47e0a
Sahara109 <np03cs4a220366@heraldcollege.edu.np>
2025-03-29 21:23:03 +0545

    Refined Module 4

--- backend/index.js
+++ backend/index.js
@@ -29,3 +29,7 @@ mongoose.connect(mongoURI)
     .then(() => console.log('MongoDB has been connected successfully'))
     .catch(err => console.error('MongoDB connection error:', err));
 
+// Start the server
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
